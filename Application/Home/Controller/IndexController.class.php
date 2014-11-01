ff020b9a01ea84861767a33ed48e5bdddc895a73
Boxin Wang <me@co8bit.com>
2014-11-01 16:50:14 +0800

    添加了ask

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,11 +1,26 @@
 <?php
 namespace Home\Controller;
+
 use Think\Controller;
+
 class IndexController extends Controller {
-    public function index(){
-
-
+    public function index()
+    {
     	$this->show("去帮助！");
         
+    }
+
+    public function ask()
+    {
+    	$data	=	null;
+
+    	$data[id]		=	$this->_param("ID");
+    	$data[title]	=	$this->_param("title");
+    	$data[type]	=	$this->_param("type");
+    	$data[content]	=	$this->_param("content");
+    	$data[time]	=	$this->_param("time");
+    	$data[uid]	=	$this->_param("uid");
+    	
+    	$D("Problem")->add($data);
     }
 }
